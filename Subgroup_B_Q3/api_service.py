60453a4fd20994970d387018276f18da64040518
Zhi Chao <limzhichao88@gmail.com>
2024-11-10 12:26:41 +0800

    Updated plotting function

--- Subgroup_B_Q3/api_service.py
+++ Subgroup_B_Q3/api_service.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from prophet.serialize import model_from_json
 from pydantic import BaseModel
+from prophet.serialize import model_from_json
 
 app = FastAPI()
 
@@ -10,16 +10,17 @@ class InferenceData(BaseModel):
 
 @app.post("/make_predictions/")
 def make_predictions(data: InferenceData):
-
-    inference_data = data.model_dump()
-
-    with open(f'./Subgroup_B_Q3/models/serialized_model_{inference_data["supplier_name"]}.json', 'r') as f:
-        prophet_model = model_from_json(f.read())
-
-    prophet_predicted = prophet_model.predict(
-        prophet_model.make_future_dataframe(
-            periods=inference_data["n_days"], include_history=False
-        )
-    )["yhat"].reset_index(drop=True)
-
-    return {"predictions": prophet_predicted.to_json()}
+    pass
+    #
+    # inference_data = data.model_dump()
+    #
+    # with open(f'./Subgroup_B_Q3/models/serialized_model_{inference_data["supplier_name"]}.json', 'r') as f:
+    #     prophet_model = model_from_json(f.read())
+    #
+    # prophet_predicted = prophet_model.predict(
+    #     prophet_model.make_future_dataframe(
+    #         periods=inference_data["n_days"], include_history=False
+    #     )
+    # )["yhat"].reset_index(drop=True)
+    #
+    # return {"predictions": prophet_predicted.to_json()}
